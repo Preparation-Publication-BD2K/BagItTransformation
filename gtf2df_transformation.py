68c5b08e87c52dc6dd6c343a1918d821679132fb
sunny <rahmansunny071@google.com>
2016-04-21 17:46:09 -0500

    normalized matrix

--- gtf2df_transformation.py
+++ gtf2df_transformation.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 import os
+import math
 #import time
 def transform(col1,colLs1,col2,colLs2,src,dest,annotations,structures):
 
-	print "Read"
+	#print "Read"
 	#start = time.clock()
 	dictList = {}
 	index = 0
+	scoreSumPerGene = {}
+	scoreSqSumPerGene = {}
+	countPerGene = {}
 	for annot in annotations:
 		#header =  col1
 		for f in annot:
@@ -42,10 +46,19 @@ def transform(col1,colLs1,col2,colLs2,src,dest,annotations,structures):
 						for elem2 in range(len(temp2)):
 							#print temp2[elem2]
 							if col2 in temp2[elem2]:
-								fpkm = temp2[elem2].rstrip().lstrip().split(" ")[1]
+								fpkm = float(temp2[elem2].rstrip().lstrip().split(" ")[1].replace("\"",''))
 					
 						if len(gid)>0: #if gene id non-empty
-							fpkmDict[gid] = fpkm
+
+							fpkmDict[gid] = fpkm 
+							if gid not in countPerGene:
+								countPerGene[gid] = 1
+								scoreSumPerGene[gid] = float(fpkm)
+								scoreSqSumPerGene[gid] = float(fpkm)*float(fpkm)
+							else:
+								countPerGene[gid] = countPerGene[gid]+1
+								scoreSumPerGene[gid] = scoreSumPerGene[gid]+float(fpkm)
+								scoreSqSumPerGene[gid] = scoreSqSumPerGene[gid]+float(fpkm)*float(fpkm) 
 					
 
 				dictList[str(f['sample'])] = fpkmDict
@@ -72,6 +85,7 @@ def transform(col1,colLs1,col2,colLs2,src,dest,annotations,structures):
 	header = header+'\n'
 	f.write(header.replace("\"",'')) 
 
+	
 	### each row
 	for d in dictList:
 		#line = d
@@ -80,7 +94,18 @@ def transform(col1,colLs1,col2,colLs2,src,dest,annotations,structures):
 			if key in dictList[d]:
 				#line = line+"\t"+str(dictList[d][key])
 				f.write('\t')
-				f.write(dictList[d][key].replace("\"",''))
+
+				mean = scoreSumPerGene[key]/countPerGene[key]
+				sqVal = (scoreSumPerGene[key]*scoreSumPerGene[key])/countPerGene[key]
+				
+
+				if sqVal < scoreSqSumPerGene[key]:
+					stdev = math.sqrt((scoreSqSumPerGene[key] - sqVal)/countPerGene[key])
+					normalizedValue = abs(dictList[d][key]-mean)/stdev
+				else:
+					normalizedValue = dictList[d][key]
+
+				f.write(str(normalizedValue))
 			else:
 				#line = line+"\t "
 				f.write('\t ')
